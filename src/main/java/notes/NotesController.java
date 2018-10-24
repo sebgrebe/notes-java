2471dba66bc28af6d4202aed9a12eb7169dcee6b
Sebastian <sebbecker@gmx.net>
2018-10-24 15:43:49 +0100

    initial commit

---
+++ src/main/java/notes/NotesController.java
@@ -0,0 +1,18 @@
+package notes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class NotesController {
+
+    @GetMapping("/")
+    public String notes(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        String title = "Notes App";
+        model.addAttribute("title", title);
+        return "index";
+    }
+
+}
