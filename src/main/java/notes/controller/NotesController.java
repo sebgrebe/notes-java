e58728f1d89859ccae2c41505f8714103b46035b
Sebastian <sebbecker@gmx.net>
2018-10-29 10:04:02 +0000

    last

--- src/main/java/notes/controller/NotesController.java
+++ src/main/java/notes/controller/NotesController.java
@@ -8,11 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class NotesController {
 
-    @RequestMapping(value = "/")
-    public String notes(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        String title = "Notes App";
-        model.addAttribute("title", title);
-        return "index";
-    }
+
 
 }
